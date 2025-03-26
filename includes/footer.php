f1246e06d00f9fd0464ae512946df3a6a8087eb4
licoshet <panickerabhiraj@gmail.com>
2025-03-26 23:31:56 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - Hostel Management System - Developed by <a href="https://www.instagram.com/abhiraj_._05/">Abhiraj Panicker</a>
+</footer>
